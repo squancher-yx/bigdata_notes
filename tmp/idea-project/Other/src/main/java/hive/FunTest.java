754c508d422d1f0980f1d59c6420bd17ce861547
Squancher <32325191+squancher-yx@users.noreply.github.com>
2021-02-27 16:23:11 +0800

    parse sql

--- tmp/idea-project/Other/src/main/java/hive/FunTest.java
+++ tmp/idea-project/Other/src/main/java/hive/FunTest.java
@@ -1,7 +1,19 @@
 package hive;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class FunTest {
     public static void main(String[] args) {
-        System.out.println("werwf");
+        JSONObject object = new JSONObject();
+        JSONObject child = new JSONObject();
+        ArrayList<String> list = new ArrayList<>();
+        list.add("dfgdfh");
+        child.put("sdf",list);
+        object.put("123",child);
+        System.out.println(object.toJSONString());
     }
 }
--- tmp/idea-project/Other/src/main/java/hive/ParseSQL.java
+++ tmp/idea-project/Other/src/main/java/hive/ParseSQL.java
@@ -1,32 +1,75 @@
 package hive;
 
+import com.alibaba.fastjson.JSONObject;
 import org.apache.hadoop.hive.ql.parse.ASTNode;
 import org.apache.hadoop.hive.ql.parse.HiveParser;
 import org.apache.hadoop.hive.ql.parse.ParseDriver;
 import org.apache.hadoop.hive.ql.parse.ParseException;
 
+import java.util.*;
+
 public class ParseSQL {
     public static void main(String[] args) throws ParseException {
         ParseDriver pd = new ParseDriver();
-        ASTNode ast = pd.parse("SELECT Q.C,T FROM ( SELECT * FROM B ) AS A");
-        parseChild(ast);
+        ASTNode ast = pd.parse("SELECT A.C as CC,T FROM ( SELECT * FROM B ) AS A");
+//        System.out.println(ast.getChild(0).getChild(0));
+//        System.out.println(ast.getChild(0).getChild(1));
+//        parseChild(ast);
+
+        parse(ast);
     }
 
 
-    public static void parseChild(ASTNode ast){
-        parseCurrent(ast);
+    public static void parseChild(ASTNode ast, Stack<ASTNode> stack) {
+//        parseCurrent(ast);
         int numCh = ast.getChildCount();
+        System.out.println(numCh);
         if (numCh > 0) {
             for (int num = 0; num < numCh; num++) {
                 ASTNode child = (ASTNode) ast.getChild(num);
-//                System.out.print(child.getText()+"    ");
-                parseChild(child);
+                System.out.print(child + " ");
             }
             System.out.println();
         }
+        if (numCh > 0) {
+            for (int num = 0; num < numCh; num++) {
+                ASTNode child = (ASTNode) ast.getChild(num);
+                parseChild(child, stack);
+            }
+        }
 
     }
-    public static void parseCurrent(ASTNode ast){
-        System.out.println(ast.getText());
+
+    public static void parse(ASTNode ast) {
+        JSONObject object = new JSONObject();
+        LinkedList<ASTNode> astList = new LinkedList<>();
+        LinkedList<JSONObject> jsonList = new LinkedList<>();
+        astList.push(ast);
+        jsonList.push(object);
+        while (!astList.isEmpty()) {
+            ASTNode tmpAST = astList.getFirst();
+            JSONObject tmpJson = jsonList.getFirst();
+            String name = tmpAST.getText();
+            int numCh = tmpAST.getChildCount();
+            tmpJson.put("name",name);
+            if (numCh > 0) {
+//                JSONObject children = new JSONObject();
+                ArrayList<JSONObject> children = new ArrayList<>();
+                for (int num = 0; num < numCh; num++) {
+                    ASTNode child = (ASTNode) tmpAST.getChild(num);
+                    astList.addLast(child);
+                    JSONObject tmpChildren = new JSONObject();
+                    tmpChildren.put("name",child.getText());
+                    children.add(tmpChildren);
+                    jsonList.addLast(tmpChildren);
+//                    System.out.print(child + " ");
+                }
+                tmpJson.put("children",children);
+//                System.out.println();
+            }
+            astList.removeFirst();
+            jsonList.removeFirst();
+        }
+        System.out.println(object.toJSONString());
     }
 }
